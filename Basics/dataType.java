b71d2c93d61f1adc07a6a8de5049a8194fb535e7
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-11-05 20:55:25 +0000

    Revised Java Data Type Examples

--- Basics/dataType.java
+++ Basics/dataType.java
@@ -4,14 +4,14 @@ public class dataType {
   public static void main(String[] args) {
     // Primitive Data Types
     int myNum = 5; // Integer
-    float myFloatNum = 5.99f; // Floating point number
+    double myDoubleNum = 5.99; // Floating point number
     char myLetter = 'D'; // Character
     boolean myBool = true; // Boolean
     String myText = "Hello"; // String
 
     // Print the values
     System.out.println("Integer: " + myNum);
-    System.out.println("Float: " + myFloatNum);
+    System.out.println("Double: " + myDoubleNum);
     System.out.println("Character: " + myLetter);
     System.out.println("Boolean: " + myBool);
     System.out.println("String: " + myText);
