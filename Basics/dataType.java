64b993f49047f67da24e0b1f9240390a7428c166
armanrahmanbd <armanrahmanbd7@gmail.com>
2025-11-05 20:49:59 +0000

    Java Data Type

---
+++ Basics/dataType.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class dataType {
+  public static void main(String[] args) {
+    // Primitive Data Types
+    int myNum = 5; // Integer
+    float myFloatNum = 5.99f; // Floating point number
+    char myLetter = 'D'; // Character
+    boolean myBool = true; // Boolean
+    String myText = "Hello"; // String
+
+    // Print the values
+    System.out.println("Integer: " + myNum);
+    System.out.println("Float: " + myFloatNum);
+    System.out.println("Character: " + myLetter);
+    System.out.println("Boolean: " + myBool);
+    System.out.println("String: " + myText);
+  }
+}
